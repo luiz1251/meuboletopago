24764d5720c946d6e59ff5b017308e1530afac4e
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-02-07 16:27:45 -0300

    Teste commit

--- shared/src/commonMain/kotlin/br/com/digitalhouse/meuboletopago/model/Login.kt
+++ shared/src/commonMain/kotlin/br/com/digitalhouse/meuboletopago/model/Login.kt
@@ -3,5 +3,5 @@ package br.com.digitalhouse.meuboletopago.model
 class Login ( val login:String, val senha:String) {
     fun validador() = (login == "usuario@kmm.com" && senha == "12345")
 
-}
+}///
 //indicação da ide para melhorar o codigo quando marcar de amarelo
